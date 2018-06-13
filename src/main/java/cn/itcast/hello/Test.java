4cfe33e9067720dd4b64effc1fa300427eec571d
gaoxing <gxwy1210@163.com>
2018-06-13 23:39:10 +0800

    hehasdfadf

--- src/main/java/cn/itcast/hello/Test.java
+++ src/main/java/cn/itcast/hello/Test.java
@@ -3,7 +3,7 @@ package cn.itcast.hello;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("hello ni renmen world");
+		System.out.println("hello ni rensdfgsdfgsmen world");
 	}
 
 }
